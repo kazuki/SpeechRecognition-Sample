2ee9a42df6567a2a4907f5b71ce697ef1d130f34
Kazuki Oikawa <kazuki.oikawa@nttdata.com>
2021-02-01 01:28:33 +0900

    Make Google Speech-to-Text specific option configurable

--- asr-client/GoogleSpeechToText.ts
+++ asr-client/GoogleSpeechToText.ts
@@ -1,13 +1,16 @@
 import { SpeechRecognitionBase } from './SpeechRecognitionBase';
 
 export class GoogleSpeechToText extends SpeechRecognitionBase {
+  enableAutomaticPunctuation = true;
+  singleUtterance = false;
+
   protected getEngineConfig(): any { // eslint-disable-line
     return {
       lang: this.lang,
       continuous: this.continuous,
       max_alternatives: this.maxAlternatives,
-      enable_automatic_punctuation: true,
-      single_utterance: false,
+      enable_automatic_punctuation: this.enableAutomaticPunctuation,
+      single_utterance: this.singleUtterance,
       interim_results: this.interimResults,
     };
   }
